3d14d47f3d89a52620d8115b9786117f72d8ebe3
gfisaris <gfisaris@gmail.com>
2016-11-18 17:39:54 +0000

    Major Refactoring and Resources Renaming

--- aws-efs-file_systems.tf
+++ aws-efs-file_systems.tf
@@ -1,12 +1,10 @@
 resource "aws_efs_file_system" "ecs_common" {
   creation_token = "ecs_common"
-  
+
   tags {
     Name = "ecs_common"
   }
 
 }
 
-output "FS-ECS_Common-ID" {
-  value = "${aws_efs_file_system.ecs_common.id}"
-}
+output "filesystem_ecs_common_id"   {value = "${aws_efs_file_system.ecs_common.id}"}
--- aws-efs-mount_targets.tf
+++ aws-efs-mount_targets.tf
@@ -1,6 +1,8 @@
 resource "aws_efs_mount_target" "ecs_common_pubnet" {
   #count = "${data.terraform_remote_state.aws_vpc.number_of_public_subnets}"
-  count = 2 
+  count = 2
   file_system_id = "${aws_efs_file_system.ecs_common.id}"
-  subnet_id = "${data.terraform_remote_state.aws_vpc.vpc_subnets_public[count.index]}"
+
+  subnet_id       = "${data.terraform_remote_state.aws_vpc.ecs_cluster_vpc_subnet_public_id[count.index]}"
+  security_groups = ["${data.terraform_remote_state.aws_vpc.security_group_efs_ecs_cluster_id}"]
 }
